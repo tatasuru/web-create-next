56b1398dac995fe0803c24c67d99fd5d8dc3a70d
小池　建 <t.koike@roseaupensant.jp>
2022-10-21 20:32:05 +0900

    nav.js修正

--- components/nav.js
+++ components/nav.js
@@ -8,7 +8,7 @@ export default function Nav() {
         <ul className={styles.list}>
           <li>
             <Link href="/">
-              <a>変更しました</a>
+              <a>TOP</a>
             </Link>
           </li>
           <li>
